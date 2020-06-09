bedb1731bdd5d94d47278c6eef7f944dda12f0ed
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-06-09 15:12:10 -0700

    started on ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,41 @@
+class Graph:
+
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex does not exist")
+        
+
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        return self.vertices[vertex_id]
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    g = Graph()
+    all_nums = set()
+    for pair in ancestors:
+        if pair[0] not in all_nums:
+            all_nums.add(pair[0])
+            g.add_vertex(pair[0])
+        if pair[1] not in all_nums:
+            all_nums.add(pair[1])
+            g.add_vertex(pair[1])
+    return g.vertices
+
+print(earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)], 7))
